1535dc34cd8a18b22f03d8135ccb9a4dac1822c6
Jullury <jullandriamazava@gmail.com>
2025-09-25 09:40:32 +0300

    chore: add lint-staged and husky pre-commit hook for code quality checks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,17 +8,18 @@ import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import globals from "globals";
 import js from "@eslint/js";
 
-const {
-    FlatCompat,
-} = require("@eslint/eslintrc");
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
     recommendedConfig: js.configs.recommended,
     allConfig: js.configs.all
 });
 
-module.exports = defineConfig([{
+export default defineConfig([{
     languageOptions: {
         parser: tsParser,
         ecmaVersion: 2022,
